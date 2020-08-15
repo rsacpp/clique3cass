bcbb2e2afcf93ecd8cfac1976db03b45acbe85e0
rsacpp <rsacpp@gmail.com>
2020-08-15 17:18:09 +0800

    rm comment

--- data3/issuertemp3.cpp
+++ data3/issuertemp3.cpp
@@ -1,22 +1,3 @@
-/**
-* issuertemp.cpp for Clique3
-* Copyright (C) 2018, Gu Jun
-*
-* This file is part of Clique3.
-* Clique3 is  free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or (at
-* your option) any later version.
-
-* Clique3 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-
-* You should have received a copy of the GNU General Public License
-* along with Clique3. If not, see <http://www.gnu.org/licenses/>.
-**/
-
 #include "bn40.h"
 #include <cstdlib>
 #include <cstring>
@@ -48,41 +29,32 @@ string rtrim0(string a){
  */
 string* digest(string* input){
   using namespace std;
-
   if((input->find("5e5e") !=0 )||(input->rfind("2424") + 4 != input->length())){
     delete input;
-
     cout<<"format error"<<endl;
     return NULL;
   }
-
   string* key = new string("KEY");
   int pos = key->find("@@");
   string* pq = new string(key->substr(0,pos));
   string* id = new string(key->substr(pos + 2));
   bn40* _cre = fromhex(id);
-
   bn40* _val = fromhex(input);
   bn40* _pq = fromhex(pq);
   bn40* r = npmod(_val,_cre,_pq);
-
   string* r2 = r->tohex();
-
-
   delete r2;
-  delete _pq;
   delete r;
+  delete _pq;
   delete _val;
-  delete pq;
   delete _cre;
-  delete input;
   delete id;
+  delete pq;
   delete key;
-
+  delete input;
   string* rr = new string(*pq + "@@" + *r2);
   return rr;
 }
-
 
 int
 main(int argc, char* argv[]){
