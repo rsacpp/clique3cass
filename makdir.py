81e8f8ff6423f039abb506b0077572a9773244ea
rsacpp <rsacpp@gmail.com>
2020-08-18 15:13:37 +0800

    use string

--- makdir.py
+++ makdir.py
@@ -1,9 +1,13 @@
 from sys import argv
 import os
-aa = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+import string
+h = string.hexdigits.lower()
 
-for a in aa:
-    for b in aa:
+for i in range(0, 16):
+    a = h[i:i+1]
+    for j in range(0, 16):
+        #print('i= {0}, j = {1}'.format(i ,j))
+        b = h[j: j+1]
         cmd = 'mkdir {0}/{1}{2}'.format(argv[1], a, b)
         print(cmd)
         os.popen(cmd)
