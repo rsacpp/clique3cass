e4cf3dab2e3920b91ecc09d388d1e4741901b608
rsacpp <rsacpp@gmail.com>
2020-08-28 09:58:26 +0800

    use boost hex() function

--- data3/verify3.cpp
+++ data3/verify3.cpp
@@ -1,31 +1,15 @@
-/**
-* bn40.cpp for Clique3
-* Copyright (C) 2018, Gu Jun
-* 
-* This file is part of Clique3.
-* Clique3 is  free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or (at
-* your option) any later version.
-
-* Clique3 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-
-* You should have received a copy of the GNU General Public License
-* along with Clique3. If not, see <http://www.gnu.org/licenses/>.
-**/
-
 #include "bn40.h"
-#include <cstring>
-using namespace std;
+#include <boost/algorithm/hex.hpp>
 
 int
 main(int argc, char* argv[]){
+  if(argc < 2){
+    cout << "usage: ./verify3 {XXXX@@XXXX}" << endl;
+    return 0;
+  }
   string* input = new string(argv[1]);
   int pos = input->find("@@");
-  if(argc < 2 || pos < 0){
+  if(pos < 0){
     cout << "usage: ./verify3 {XXXX@@XXXX}" << endl;
     delete input;
     return 0;
@@ -38,34 +22,13 @@ main(int argc, char* argv[]){
   bn40* _v  = fromhex(v);
   bn40* r = npmod(_v,_e,_pq);
   string* rr = r->tohex();
-  /*cout <<*rr<<endl;
-   */
-  if(rr->find("e500e500") !=0 ){
-    cout<<"wrong code"<<endl;
-  }else{
-    char* codes = new char[rr->length() + 1];
-    std::strcpy(codes,rr->c_str());
-    for(int i = 0; i < rr->length(); i = i + 4){
-      char val = 0;
-      for(int j = 0; j < 4; j++){
-	char ch = codes[i + j];
-	if((ch >= '0') &&( ch <='9')){
-	  val+= ((ch - '0')<<(j*4));
-	}
-	if((ch >= 'a') && (ch <='f')){
-	  val += ((ch - 'a' + 10)<<(j*4));
-	}
-      }
-      cout<<val;
-    }
-    cout<<endl;
-    delete[] codes;
-  }
+  string code = boost::algorithm::unhex(*rr);
+  cout<<code<<endl;
   delete rr;
   delete r;
-  delete _pq;
-  delete _e;
   delete _v;
+  delete _e;
+  delete _pq;
   delete v;
   delete pq;
   delete input;
