a0ba76ae3d3c397ead51a41eed694b3e75c33dd0
EC2 Default User <ec2-user@ip-172-31-38-31.ap-northeast-1.compute.internal>
2020-10-22 05:56:44 +0800

    step1 is a duplication of crypt, not needed any more

--- data3/make.pl
+++ data3/make.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
-system('g++ step1.cpp bn40.cpp -o step1');
+# system('g++ step1.cpp bn40.cpp -o step1');
 system('g++ step2.cpp bn40.cpp -o step2');
 system('g++ crypt.cpp bn40.cpp -o crypt');
